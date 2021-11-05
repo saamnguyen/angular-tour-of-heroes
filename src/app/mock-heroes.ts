f2bf8068898e1e331ebe6db91a2c171fcb15bb7d
samukma <nguyensam576@gmail.com>
2021-11-05 17:56:40 +0700

    add hero-detail component

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import {Hero} from "./hero";
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Spider Man'},
+  {id: 12, name: 'Dr. Strange'},
+  {id: 14, name: 'Iron Man'},
+  {id: 15, name: 'Thanos'},
+  {id: 16, name: 'Son Tung M-TP'},
+  {id: 17, name: 'Kha Banh'},
+  {id: 18, name: 'Captain American'},
+  {id: 19, name: 'Dr IQ'},
+  {id: 20, name: 'Magna'},
+];
