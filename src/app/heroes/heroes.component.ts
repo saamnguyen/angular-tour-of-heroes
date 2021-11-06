e22087416f2b67a4263bf4a23b631c91bdcbc493
samukma <nguyensam576@gmail.com>
2021-11-06 16:34:35 +0700

    done project

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,8 +11,6 @@ import { MessageService} from "../message.service";
 })
 export class HeroesComponent implements OnInit {
   heroes : Hero[] = [];
-  selectedHero? : Hero;
-
 
   constructor(private heroService: HeroService, private messageService: MessageService) { }
 
@@ -20,15 +18,23 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero; //Gán lại cho selectHero = hero vốn dĩ selectHero  = undefined
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
-    // console.log(this.selectedHero);
-  }
-
   getHeroes(): void{
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
   }
 
 }
